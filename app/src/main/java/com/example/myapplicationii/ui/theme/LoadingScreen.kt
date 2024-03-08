809bcf13e6be3fe7f2790bf3645e78719a8b8dfd
fabiosilva <f.luiz.10.1@gmail.com>
2024-03-07 20:24:23 -0400

    Iniciando tela de carregamento.

---
+++ app/src/main/java/com/example/myapplicationii/ui/theme/LoadingScreen.kt
@@ -0,0 +1,13 @@
+package com.example.myapplicationii.ui.theme
+
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+
+@Composable
+fun LoadingScreen {
+    Surface(
+
+    )
+
+    }
+}
